321df9c9a782673cd15b4f9a5548ead5a1278916
cookieisgood <alan.yuan.jly@gmail.com>
2019-03-13 21:41:08 -0400

    completed register and stuff for servers

---
+++ server_code/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const mongoose = require('mongoose');
+
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
+const isAuth = require('./middleware/is-auth');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(isAuth);
+
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
+    graphiql: true
+  })
+);
+
+
+
+mongoose
+
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-6ujpq.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`, { useNewUrlParser: true }
+  )
+  .then(() => {
+    app.listen(3000, () => console.log('Server running on port 3000!'))  
+  })
+  .catch(err => {
+    console.log(err);
+  });
+    
+
+// Start the Express server
